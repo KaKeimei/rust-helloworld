695078e4484c55e7d67eb94bd417d00ed6687513
KaKeimei <hqm237594742@gmail.com>
2022-04-28 13:45:41 +0900

    test ownership

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    ownership_test()
+}
+
+fn ownership_test() {
+    let mut s1 = String::from("hello");
+    // 函数调用将转移所有权，返回值会交还所有权
+    s1 = takes_ownership_and_give_back(s1);
+    println!("s1 = {}", s1);
+}
+
+fn takes_ownership_and_give_back(some_string: String) -> String{
+    print!("{}", some_string);
+    return some_string
 }
