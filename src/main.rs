53e5a7abbce58a359a49f3f463da5fdd90b63bc7
KaKeimei <hqm237594742@gmail.com>
2022-05-02 16:39:04 +0900

    finish array

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,9 @@ fn main() {
 
     // 测试枚举类型
     enum_test();
+
+    // 测试数组
+    array_test();
 }
 
 fn ownership_test() {
@@ -140,7 +143,6 @@ fn struct_test() {
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
-    Write(String),
     ChangeColor(i32, i32, i32),
 }
 
@@ -151,4 +153,33 @@ fn enum_test() {
     // todo 获取枚举成员内部的数据
     let m3 = Message::ChangeColor(255, 255, 0);
     println!("{:?}, {:?}, {:?}", m1, m2, m3)
+}
+
+fn array_test() {
+    // 编译器自动推导出one的类型
+    let one = [1, 2, 3];
+    // 显式类型标注
+    let two: [u8; 3] = [1, 2, 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    // arrays是一个二维数组，其中每一个元素都是一个数组，元素类型是[u8; 3]
+    let arrays: [[u8; 3]; 4] = [one, two, blank1, blank2];
+
+    // 借用arrays的元素用作循环中
+    for a in &arrays {
+        print!("{:?}: ", a);
+        // 将a变成一个迭代器，用于循环
+        // 你也可以直接用for n in a {}来进行循环
+        for n in a.iter() {
+            print!("\t{} + 10 = {}", n, n + 10);
+        }
+
+        let mut sum = 0;
+        // 0..a.len,是一个 Rust 的语法糖，其实就等于一个数组，元素是从0,1,2一直增加到到a.len-1
+        for i in 0..a.len() {
+            sum += a[i];
+        }
+        println!("\t({:?} = {})", a, sum);
+    }
 }
