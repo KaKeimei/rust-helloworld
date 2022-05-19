66d41a7b8942a1d644605f1ebb90a43be8220444
KaKeimei <hqm237594742@gmail.com>
2022-05-19 18:45:04 +0900

    finish error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::fmt::{Debug, Display, Error, Formatter};
-use std::ops;
+use std::fs::File;
+use std::io::{ErrorKind, Read};
 use std::ops::Mul;
+use std::{io, ops};
 
 fn main() {
     println!("Hello, world!");
@@ -49,6 +51,9 @@ fn main() {
 
     // 测试类型转换
     cast_test();
+
+    // 测试错误处理
+    error_test();
 }
 
 fn ownership_test() {
@@ -728,4 +733,37 @@ fn cast_test() {
     if a < b_ {
         println!("Ten is less than one hundred.");
     }
+}
+
+fn error_test() {
+    let f = File::open("hello.txt");
+
+    match f {
+        Ok(file) => {
+            drop(file);
+        }
+        Err(error) => match error.kind() {
+            // 选取错误类型
+            ErrorKind::NotFound => {
+                println!("{:?}", error)
+            }
+            _ => {
+                println!("other error")
+            }
+        },
+    }
+    match read_username_from_file() {
+        Ok(s) => {
+            println!("{}", s)
+        }
+        Err(err) => {
+            println!("{:?}", err)
+        }
+    }
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s);
+    Ok(s)
 }
